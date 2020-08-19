2c2c00b9cd308a263595f0d27d8964401bb548fe
Nathan Blankson <nathan@sourcebreaker.com>
2020-08-19 21:21:16 +0100

    JWT auth guard

--- src/+forecast/controller/forecast.controller.ts
+++ src/+forecast/controller/forecast.controller.ts
@@ -1,20 +1,23 @@
 // Nest dependencies
-import { Controller, Get, Query, Post, Body } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body, UseGuards } from '@nestjs/common';
 
 // Local files
 import { ForecastService } from '../service/forecast.service';
 import { Location } from '../../core/models/location.interface';
+import { JwtAuthGuard } from '../../core/guards/jwt-auth.guard';
 
 @Controller('forecast')
 export class ForecastController {
     constructor(private _forecastService: ForecastService) { }
 
     @Get()
+    @UseGuards(JwtAuthGuard)
     forecastBasicSearch(@Query('q') query: string) {
         return this._forecastService.getForecastBasicSearch(query);
     }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     forecastAdvancedSearch(@Body() query: Location) {
         return this._forecastService.getForecastAdvancedSearch(query);
     }
